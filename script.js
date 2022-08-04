2d1486f06ef69d28cf0046e9a0b84766a6e584e0
Andrew-55 <tap55rus@gmail.com>
2022-08-04 10:44:57 +0600

    Andrew.test_building_diagrams

--- script.js
+++ script.js
@@ -29,62 +29,159 @@ class ItcAccordion {
 
 new ItcAccordion("#accordion-1");
 
-const ctxOne = document.getElementById('myChart_one');
+const ctxOne = document.getElementById("myChart_one").getContext('2d');
 const myChart_one = new Chart(ctxOne, {
-    type: 'doughnut',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [165, 140, 30],
+        backgroundColor: ["#E83C46", "#F9A620", "#474444"],
+        borderColor: ["#E83C46", "#F9A620", "#474444"],
+        borderWidth: 1,
+      },
+    ],
+    labels: ["Россия", "Казахстан", "Узбекистан"],
+  },
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: "Страны",
+        align: "start",
+      },
+      legend: {
+        position: "bottom",
+        labels: {
+          point: "circle",
+        }
+      },
     },
-    options: {}
+    cutout: 90,
+    rotation: -90,
+    legend: {
+      position: "right",
+    },
+  },
 });
 
-const ctxTwo = document.getElementById('myChart_two');
-const myChart_two = new Chart(ctxTwo, {
-    type: 'doughnut',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+Highcharts.chart("container", {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: 0,
+    plotShadow: false,
+  },
+  title: {
+    text: "Browser<br>shares<br>2017",
+    align: "center",
+    verticalAlign: "middle",
+    y: 60,
+  },
+  accessibility: {
+    point: {
+      valueSuffix: "%",
     },
-    options: {}
+  },
+  plotOptions: {
+    pie: {
+      dataLabels: {
+        enabled: true,
+        distance: -50,
+        style: {
+          fontWeight: "bold",
+          color: "white",
+        },
+      },
+      startAngle: -90,
+      endAngle: 90,
+      center: ["50%", "75%"],
+      size: "110%",
+    },
+  },
+  series: [
+    {
+      type: "pie",
+      name: "Browser share",
+      innerSize: "50%",
+      data: [
+        ["Chrome", 58.9],
+        ["Firefox", 13.29],
+        ["Internet Explorer", 13],
+        ["Edge", 3.78],
+        ["Safari", 3.42],
+        {
+          name: "Other",
+          y: 7.61,
+          dataLabels: {
+            enabled: false,
+          },
+        },
+      ],
+    },
+  ],
+});
+
+const ctxThree = document.getElementById("myChart_three");
+const myChart_three = new Chart(ctxThree, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {},
+});
+
+const ctxFour = document.getElementById("myChart_four");
+const myChart_four = new Chart(ctxFour, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {},
 });
